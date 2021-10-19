c1140fe79f526ec9301f28736cf66e1a73ca1f16
pasyagitka <lizavetazinovich@gmail.com>
2021-10-19 14:58:56 +0300

    03 06 remove fork

--- 03/linux/os03-06.c
+++ 03/linux/os03-06.c
@@ -5,16 +5,17 @@
 
 int main()
 {
-	if(fork() == 0)
-	{
-		system("./5_1");
-		exit(0);
-	}
+	printf("Start parent process\n");
+	printf("5_1 process\n");
+	system("./5_1");
+
+	printf("main process\n");
 	pid_t pid = getpid();
 	for (int i = 1; i<100; i++)
 	{
 		sleep(1);
 		printf("os03-06 : %d-%d\n", pid, i);
 	}
+	printf("Finish parent process\n");
 	exit(0);
 }
